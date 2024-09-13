4c7f554798b33f007e8dbb9741c3b3dd49bc48fa
Noahwi1 <noah.willi@unibas.ch>
2024-09-13 18:35:59 +0200

    Adding Repdata analysis

--- AnalysisR.R
+++ AnalysisR.R
@@ -54,6 +54,31 @@ mydata$Sex_ch <- factor(mydata$Sex, levels=c(0,1), labels=c("female", "male"))
 summary(mydata)
 summary(lm(Extraversion ~ Sex, data=mydata))
 
+#repdata:
+View(repdata)
+dim(repdata)
+summary(repdata)
+head(repdata)
+
+#There is a column «Filter», delete all participants with the entry «1».
+#How many participants are left after removing all with Filter = 1?
+repdata$Sex <- as.factor(repdata$Sex)
+repdata$Filter <- as.factor(repdata$Filter)
+summary(repdata)
+
+repdata <- subset(repdata, Filter == 0)
+nrow(repdata)
+
+t.test(repdata$Testosteron ~ repdata$Sex)
+plot(repdata$Testosteron ~ repdata$Sex)
+summary(repdata$Testosteron[repdata$Sex==0])
+summary(repdata$Testosteron[repdata$Sex==1])
+
+
+repdata$Sex_ch <- factor(repdata$Sex, levels=c(0,1), labels=c("female", "male"))
+summary(repdata)
+
+
 #Reliability: For the memory data, you have a repeated measurement (SD = short
 #delay, after 10 min & LD = long delay – after 1 day). How high is the correlation
 #between these two measurements? Is there a performance difference between SD and LD?
@@ -67,6 +92,12 @@ summary(mydata$EM_LD)
 #but good correlation, because of the 1 day delay. The performance difference is also
 #to be expected. 
 
+#repdata:
+cor(repdata$EM_SD, repdata$EM_LD, use="pairwise") #Correlation: 0.8684151
+t.test(repdata$EM_SD, repdata$EM_LD) # p-value = 4.475e-07
+summary(repdata$EM_SD) #mean 28.17
+summary(repdata$EM_LD) #mean 23.46 > showing the same pattern
+
 
 #Aggregation: Since the SD and LD memory performance are highly correlated, 
 #you can calculate also the average memory performance for the downstream analysis 
@@ -75,6 +106,12 @@ mydata$EM <- (mydata$EM_SD + mydata$EM_LD)/2
 plot(density(mydata$EM_SD), main="Compare EM SD and LD", frame.plot=F)
 lines(density(mydata$EM_LD), col="red")
 lines(density(mydata$EM), col="green")
+
+#repdata
+repdata$EM <- (repdata$EM_SD + repdata$EM_LD)/2
+plot(density(repdata$EM_SD), main="Compare EM SD and LD", frame.plot=F)
+lines(density(repdata$EM_LD), col="red")
+lines(density(repdata$EM), col="green")
 
 #Validation: There is fMRI-data available from the hippocampus and amygdala, 
 #comparing the fMRI signal between negative and neutral pictures. The two brain 
@@ -86,12 +123,23 @@ abline(lm(mydata$fMRI_hipp_neg_neu ~ mydata$fMRI_amy_neg_neu))
 summary(lm(EM ~ Sex + Extraversion + fMRI_hipp_neg_neu, data=mydata))
 #correlation is 0.75
 
+#repdata
+cor(repdata$fMRI_amy_neg_neu, repdata$fMRI_hipp_neg_neu) #Corr. 0.7660941
+plot(repdata$fMRI_amy_neg_neu, repdata$fMRI_hipp_neg_neu, pch=19)
+abline(lm(repdata$fMRI_hipp_neg_neu ~ repdata$fMRI_amy_neg_neu))
+
+summary(lm(EM ~ Sex_ch + Extraversion + fMRI_hipp_neg_neu, data=repdata))
+#Same as mydata
+
 #Statistical analysis: After finishing your validation and reliability checks, 
 #filtering of the data and also the building of new (averaged) behavioral data, 
 #you start to think about a hypothesis you can test and replicate in your datasets.
 #Add an appropriate statistical analysis to your script and run and replicate this analysis.
-repdata <- read.table("repdata.txt", header=T, sep="\t")
-repdata <- subset(repdata, Filter==0)
+#Hypothesis: Extrav and WM correlate positively
+correlation <- cor(mydata$EM_SD, mydata$Extraversion) #corr. = 0.0256
+plot(mydata$EM_SD ~ mydata$Extraversion)
+cor.test(mydata$EM_SD, mydata$Extraversion)
+
 
 #Filter Part 1: potentially separating the power analysis from the quality checks, 
 #and combining reliability, validity and objectivity in one script. 
