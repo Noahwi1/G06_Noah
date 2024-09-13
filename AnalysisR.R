50bd88c687a476aa42e41479f32141163ea2dd3f
Noahwi1 <noah.willi@unibas.ch>
2024-09-13 11:04:24 +0200

    Update AnalysisR.R

--- AnalysisR.R
+++ AnalysisR.R
@@ -1,1 +1,27 @@
-sheet 1
+#Analysis
+install.packages("pwr")
+library(pwr)
+#test different power analysis for significance level 0.1, 0.3 and 0.5
+pwr.r.test(n = 800, r = 0.5, sig.level = 0.05, power = NULL,
+           alternative = c("two.sided", "less", "greater"))
+#for 0.1 -> pwr = 0.809, for 0.3 -> pwr = 1, for 0.5 -> pwr = 1
+
+#in a next step, run 10 analyses with sig.level = 0.005
+pwr.r.test(n = 800, r = 0.5, sig.level = 0.005, power = NULL,
+           alternative = c("two.sided", "less", "greater"))
+#for 0.1 -> pwr = 0.51, for 0.3 -> pwr = 1, for 0.5 -> pwr = 1
+
+#What is the minimum sample size you need to replicate a large, medium, or a small effect with a power of 90%?
+pwr.r.test(n = NULL, r = 0.5, sig.level = 0.005, power = 0.9,
+           alternative = c("two.sided", "less", "greater"))
+#for 0.1 -> N = 1663.06, for 0.3 -> N = 177.1, for 0.5 -> N = 58.1
+
+#Your replication sample contains about 250 participants with high-quality data.
+#How much power do you have to replicate a significant finding for a small, 
+#medium or large effect (alpha=0.05, two-sided) in this sample?
+pwr.r.test(n = 250, r = 0.5, sig.level = 0.05, power = NULL,
+           alternative = c("two.sided", "less", "greater"))
+#for 0.1 -> pwr = 0.35, for 0.998 -> pwr = 1, for 0.5 -> pwr = 1
+
+
+           
